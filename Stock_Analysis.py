6355591b824957db55e24cc6ff66b58bbb1e90f3
Mahek Mehta <161959125+Anni1808@users.noreply.github.com>
2024-05-27 04:21:31 -0700

    Stock analysis

---
+++ Stock_Analysis.py
@@ -0,0 +1,27 @@
+import streamlit as st, pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import time
+
+st.header('Indian Stock Dashboard')
+
+ticker = st.sidebar.text_input('Symbol Code', 'INFY')
+exchange = st.sidebar.text_input('Exchange', 'NSE')
+
+url = f'https://www.google.com/finance/quote/{ticker}:{exchange}'
+
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+price = float(soup.find(class_='YMlKec fxKbKc').text.strip()[1:].replace(",", ""))
+prev_close = float(soup.find(class_='P6K39c').text.strip()[1:].replace(",", ""))
+revenue = soup.find(class_='QXDnM').text
+news = soup.find(class_='Yfwt5').text
+about = soup.find(class_='bLLb2d').text
+
+dict1 = {'Price' : price, 'Previous Close' : prev_close, 'Revenue' : revenue, 'News' : news, 'About' : about}
+
+df=pd.DataFrame(dict1, index=['Extracted Data']).T
+
+st.write(df)
+
